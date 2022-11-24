a03e3d506a6aad472c76c7f893f3fc8bec4d4ac7
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-24 15:18:06 +0800

    Declared a variable that will store the paragraph element that will display the result.

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ let button = document.getElementById("button");
 button.addEventListener("click", () => {
   const height = parseInt(document.getElementById("height").value);
   const weight = parseInt(document.getElementById("weight").value);
+  const result = document.getElementById("result");
 });
