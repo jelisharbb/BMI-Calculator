4373d9686f727b450cfbecdaf6fa65f542ea4caa
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-24 15:27:12 +0800

    Implemented a warning message if the user enters invalid height.

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ button.addEventListener("click", () => {
   const height = parseInt(document.getElementById("height").value);
   const weight = parseInt(document.getElementById("weight").value);
   const result = document.getElementById("result");
+  let heightStatus = false,
+    weightStatus = false;
+
+  if (isNaN(height) || height <= 0) {
+    document.getElementById("height-error").innerHTML = "*Invalid height";
+  }
 });
