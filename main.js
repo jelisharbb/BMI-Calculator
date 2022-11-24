9d9a65d343ac08f8f5962e28cd4342c1169170cc
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-24 17:21:43 +0800

    Added br tag inside the string.

--- main.js
+++ main.js
@@ -25,15 +25,15 @@ button.addEventListener("click", () => {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
     if (bmi < 18.5) {
-      result.innerHTML = "BMI: " + bmi + "Weight Status: Underweight";
+      result.innerHTML = "BMI: " + bmi + "<br>Weight Status: Underweight";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-      result.innerHTML = "BMI: " + bmi + "Weight Status: Normal";
+      result.innerHTML = "BMI: " + bmi + "<br>Weight Status: Normal";
     } else if (bmi >= 25 && bmi <= 29.9) {
-      result.innerHTML = "BMI: " + bmi + "Weight Status: Overweight";
+      result.innerHTML = "BMI: " + bmi + "<br>Weight Status: Overweight";
     } else if (bmi >= 30 && bmi <= 34.9) {
-      result.innerHTML = "BMI: " + bmi + "Weight Status: Obese";
+      result.innerHTML = "BMI: " + bmi + "<br>Weight Status: Obese";
     } else {
-      result.innerHTML = "BMI: " + bmi + "Weight Status: Extremely obese";
+      result.innerHTML = "BMI: " + bmi + "<br>Weight Status: Extremely obese";
     }
   } else {
     alert("The form has error.");
