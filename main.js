288fbc030e1e9bc21b2d22b90d2b95af22cf60ba
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-24 17:11:18 +0800

    Removed the br tag because it causes error.

--- main.js
+++ main.js
@@ -22,18 +22,18 @@ button.addEventListener("click", () => {
   }
 
   if (heightStatus && weightStatus) {
-    const bmi = weight / ((height * height) / 10000).toFixed(2);
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
     if (bmi < 18.5) {
-      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Underweight";
+      result.innerHTML = "BMI: " + bmi + "Weight Status: Underweight";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Normal";
+      result.innerHTML = "BMI: " + bmi + "Weight Status: Normal";
     } else if (bmi >= 25 && bmi <= 29.9) {
-      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Overweight";
+      result.innerHTML = "BMI: " + bmi + "Weight Status: Overweight";
     } else if (bmi >= 30 && bmi <= 34.9) {
-      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Obese";
+      result.innerHTML = "BMI: " + bmi + "Weight Status: Obese";
     } else {
-      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Extremely obese";
+      result.innerHTML = "BMI: " + bmi + "Weight Status: Extremely obese";
     }
   } else {
     alert("The form has error.");
