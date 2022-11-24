66a905a8a2e5817d8ccb3dd7c6822bef838a57ff
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-11-24 16:04:53 +0800

    Implemented an if else statement that will display the weight status.

--- main.js
+++ main.js
@@ -23,5 +23,17 @@ button.addEventListener("click", () => {
 
   if (heightStatus && weightStatus) {
     const bmi = weight / ((height * height) / 10000).toFixed(2);
+
+    if (bmi < 18.5) {
+      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Overweight";
+    } else if (bmi >= 30 && bmi <= 34.9) {
+      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Obese";
+    } else {
+      result.innerHTML = "BMI: " + bmi < br > "Weight Status: Extremely obese";
+    }
   }
 });
